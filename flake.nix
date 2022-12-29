a14022a00341b02634884f549da17ced7712b9e6
Otavio Salvador <otavio@ossystems.com.br>
2022-12-29 11:18:35 -0300

    general: infrastructure
    This is a initial commit to prepare the baseline for the development
    work.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Zephyr C++ API - Development Environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    zephyr-arm-dev = {
+      url = "github:almini/nix-zephyr-arm-dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { ... }@inputs: inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+    let
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = inputs.zephyr-arm-dev.devShells.${system}.default.overrideAttrs (final: prev: {
+        buildInputs = prev.buildInputs ++ [
+          pkgs.clang
+        ];
+      });
+    });
+}
